3c582eb5bdae01e3b29045ca19e0079e8d07e122
Jmahyals <146292993+Jmahyals@users.noreply.github.com>
2024-08-17 20:12:08 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 # Directorio donde se encuentra el repo bashUP
 REPO_DIR="$HOME/bashUP"
+MYSCRIPT_DIR="$HOME/myscript"
 
 # Verificar si el directorio del repositorio existe
 if [ -d "$REPO_DIR" ]; then
@@ -13,11 +14,16 @@ fi
 
 # Reemplazar ~/.bashrc con el bashrc del repositorio
 echo "Reemplazando ~/.bashrc con el bashrc del repositorio..."
-cp "$REPO_DIR/bashrc" ~/.bashrc
+mv "$REPO_DIR/bashrc" ~/.bashrc
 
-# Otorgar permisos a la carpeta myscript
-echo "Otorgando permisos a la carpeta ~/myscript..."
-chmod -R 755 ~/myscript
+# Verificar si la carpeta myscript existe
+if [ -d "$MYSCRIPT_DIR" ]; then
+  # Otorgar permisos de ejecución a todos los archivos shell en la carpeta myscript
+  echo "Otorgando permisos de ejecución a los archivos shell en ~/myscript..."
+  find "$MYSCRIPT_DIR" -type f -name "*.sh" -exec chmod +x {} \;
+else
+  echo "La carpeta ~/myscript no existe. Asegúrate de haberla creado y de que contenga tus scripts."
+fi
 
 # Instalar fzf, lsd y ranger
 echo "Instalando fzf, lsd y ranger..."
